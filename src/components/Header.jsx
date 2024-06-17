50b17a48e09d1380e6b5c3cd4dda63381cb734ef
Leslie Mwangi <luciannahphoi@gmail.com>
2024-06-17 17:04:00 +0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1
+        style={{
+          textAlign: "center",
+          fontSize: "4.0rem",
+          fontWeight: "bold",
+          backgroundColor: "white",
+        }}
+      >
+        <u>Real Estate App</u>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
